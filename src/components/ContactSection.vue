2c241722d1504f708650ebfebea2a76701039522
Icha Cahya Wulan <ichacahyawulan@gmail.com>
2022-09-08 01:15:14 +0700

    delete unused assets

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -25,25 +25,6 @@
     <div class="svg-border-waves text-white">
       <v-img src="~@/assets/img/borderWavesBlue.svg"/>
     </div>
-    <v-snackbar
-        v-model="snackbar.enabled"
-        timeout="3000"
-        right
-        top
-        :color="snackbar.color"
-    >
-      {{ snackbar.text }}
-
-      <template v-slot:action="{ attrs }">
-        <v-btn
-            text
-            v-bind="attrs"
-            @click="snackbar.enabled = false"
-        >
-          Fechar
-        </v-btn>
-      </template>
-    </v-snackbar>
   </section>
 </template>
 
@@ -62,52 +43,3 @@
 }
 
 </style>
-
-<script>
-// import {db} from '@/main'
-
-export default {
-  data: () => ({
-    icons: ["fa-facebook", "fa-twitter", "fa-linkedin", "fa-instagram"],
-    valid: true,
-    name: "",
-    nameRules: [
-      (v) => !!v || "O campo nome é obrigatório",
-      (v) => (v && v.length >= 6) || "O nome precisa ter mais de 6 caracteres",
-    ],
-    email: "",
-    emailRules: [
-      (v) => !!v || "O campo email é obrigatório",
-      (v) => /.+@.+\..+/.test(v) || "O E-mail precisa ser válido",
-    ],
-    textArea: "",
-    textAreaRules: [
-      (v) => !!v || "O campo de texto é obrigatório",
-      (v) => (v && v.length >= 10) || "Mínimo de 10 caracteres",
-    ],
-    lazy: false,
-    snackbar: {
-      enabled: false,
-      text: '',
-      color: ''
-    }
-  }),
-  methods: {
-    submit() {
-      /*db.collection("contactData").add({
-        name: this.name,
-        email: this.email,
-        message: this.textArea
-      }).then(() => {
-        this.snackbar.text = "Mensagem enviada com sucesso"
-        this.snackbar.color = "success"
-        this.snackbar.enabled = true
-      }).catch(() => {
-        this.snackbar.text = "Erro ao enviar mensagem"
-        this.snackbar.color = "danger"
-        this.snackbar.enabled = true
-      })*/
-    }
-  }
-};
-</script>
